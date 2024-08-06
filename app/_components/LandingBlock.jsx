44e064493148f9038454402b8997696495d69a00
pinoen <pinoen@yahoo.com.ar>
2024-08-06 12:32:07 -0300

    Adjust styling in landing page

--- app/_components/LandingBlock.jsx
+++ app/_components/LandingBlock.jsx
@@ -1,6 +1,6 @@
 export const LandingBlock = ({ service, description }) => {
   return (
-    <div className="absolute flex flex-col gap-4 bg-white rounded-lg py-2 px-4 md:px-8 md:mx-40 md:py-8 lg:w-[500px] lg:h-[246px] lg:mx-4 lg:mt-28">
+    <div className="relative top-[-100px] mt-10 flex flex-col gap-2 bg-white rounded-lg py-2 px-4 md:px-8 md:mx-40 md:py-8 lg:w-[500px] lg:h-[246px] lg:mt-28 lg:mx-4">
       <h3 className="text-3xl font-bold">{service}</h3>
       <hr className="h-[2px] bg-A3 border-A3" />
       <p>{description}</p>
--- app/_components/LandingMain.jsx
+++ app/_components/LandingMain.jsx
@@ -2,7 +2,7 @@ import { LandingBlock } from "./LandingBlock"
 
 export const LandingMain = () => {
   return (
-    <main className="bg-[url('/user.svg')] md:bg-[url('/userMd.png')] lg:bg-[url('/userLg.png')] bg-center bg-cover bg-no-repeat h-full">
+    <main className="bg-[url('/user.svg')] md:bg-[url('/userMd.png')] lg:bg-[url('/userLg.png')] bg-center bg-cover bg-no-repeat h-screen">
       <h1 className="text-white font-medium text-3xl md:font-normal md:text-[48px] w-[250px] md:w-[458px] p-5 md:pl-12 md:pt-20 lg:leading-tight">De ahora en adelante, hacés más con tu dinero</h1>
       <hr className="h-[4px] w-6 bg-A3 border-A3 my-4 ml-5 md:hidden" />
       <h2 className="text-A3 text-2xl w-[228px] md:w-full p-5 md:pl-12 lg:text-3xl">Tu nueva <span className="font-bold">billetera virtual</span></h2>
@@ -11,7 +11,7 @@ export const LandingMain = () => {
         <LandingBlock service={'Transferí dinero '} description={'Desde Digital Money House vas a poder transferir dinero a otras cuentas, asi como también recibir transferencias y nuclear tu capital en nuestra billetera virtual.'} />
         <LandingBlock service={'Pago de servicios'} description={'Pagá mensualmente los servicios en 3 simples clicks. Facil, rápido y conveniente. Olvidate de las facturas en papel.'} />
       </div>
-      <div className="bg-A3 h-[429px] md:h-[618px] lg:h-[300px] lg:absolute lg:bottom-0 w-full rounded-t-xl" src="/bgLanding.svg" alt="bg landing" />
+      <div className=" bg-A3 h-[370px] mt-24 md:h-[618px] lg:h-[300px] lg:absolute lg:bottom-0 w-full rounded-t-xl" src="/bgLanding.svg" alt="bg landing" />
     </main>
   )
 }
