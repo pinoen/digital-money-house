0786699044dfad224a18bae38864aec55d404139
pinoen <pinoen@yahoo.com.ar>
2024-09-13 22:56:29 -0300

    add default options like slateTime so to avoid unneeded requests of data

--- app/_context/ReactQueryProvider.jsx
+++ app/_context/ReactQueryProvider.jsx
@@ -2,7 +2,17 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const ReactQueryProvider = ({ children }) => {
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 30 * 60,
+        cacheTime: 1000 * 60 * 60,
+        refetchOnWindowFocus: false,
+        refetchOnReconnect: false,
+        retry: false,
+      }
+    }
+  })
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 }
 
